24e352c0d109f333187d84a1e1e15957d86cca86
mimilettd <mimilettd@gmail.com>
2018-01-05 10:59:51 -0700

    Initial commit

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://yourMongoDBURIGoesHere', { useMongoClient: true });
+mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ds241677.mlab.com:41677/roommates`, { useMongoClient: true });
