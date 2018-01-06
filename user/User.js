e0bc892f3ddb7a248a3509f562acbd2f44a8ef51
mimilettd <mimilettd@gmail.com>
2018-01-05 18:47:39 -0700

    set unique validator on mongoDB

--- user/User.js
+++ user/User.js
@@ -1,9 +1,12 @@
-var mongoose = require('mongoose');  
-var UserSchema = new mongoose.Schema({  
-  name: String,
-  email: String,
-  password: String
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+var UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, index: true, unique: true, required: true },
+  password: { type: String, required: true }
 });
 mongoose.model('User', UserSchema);
+
+UserSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User');
