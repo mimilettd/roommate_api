9ea5577ae7f4c5691d3646f61eeed44fe326a14b
mimilettd <mimilettd@gmail.com>
2018-01-06 23:22:53 -0700

    facebook oauth complete

---
+++ facebook.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+export const facebook = {
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  profileFields: ['id', 'name', 'displayName', 'photos', 'email'],
+};
+
