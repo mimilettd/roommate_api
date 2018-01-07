8e61710c37a546ef6641cfb4d2e68f31227e3120
mimilettd <mimilettd@gmail.com>
2018-01-07 12:13:35 -0700

    update facebook callback url

--- facebook.js
+++ facebook.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 export const facebook = {
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  callbackURL: 'https://salty-sea-38186.herokuapp.com/auth/facebook/callback',
   profileFields: ['id', 'name', 'displayName', 'photos', 'email', 'about', 'address', 'birthday', 'gender', 'location'],
 };
 
